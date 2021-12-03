88cb24dc5765fb08c05ee399912cc53a9fcbcdc6
0phoff <0phoff@users.noreply.github.com>
2021-12-03 18:36:46 +0100

    Version this package with miniver

--- setup.py
+++ setup.py
@@ -1,13 +1,29 @@
 import setuptools as setup
 
 
+def get_version_and_cmdclass(pkg_path):
+    """
+    Load version.py module without importing the whole package.
+    Template code from miniver
+    """
+    import os
+    from importlib.util import module_from_spec, spec_from_file_location
+
+    spec = spec_from_file_location('version', os.path.join(pkg_path, '_version.py'))
+    module = module_from_spec(spec)
+    spec.loader.exec_module(module)
+    return module.__version__, module.get_cmdclass(pkg_path)
+
+
 def find_packages():
     return ['top'] + ['top.'+p for p in setup.find_packages('top')]
 
 
+version, cmdclass = get_version_and_cmdclass(r'top')
 setup.setup(
     name='top',
-    version='0.0.0',
+    version=version,
+    cmdclass=cmdclass,
     author='0phoff',
     description='TOPCraft: Top Notch Python Debugging Tools',
     long_description=open('README.md').read(),
--- top/__init__.py
+++ top/__init__.py
@@ -1,3 +1,4 @@
+# Expose to root
 from ._gc import *
 del _gc
 
@@ -13,5 +14,10 @@ del _profile
 from ._time import *
 del _time
 
-
+# Export submodule
 from . import _meta as meta
+
+# Miniver stuff
+# https://github.com/jbweston/miniver
+from ._version import __version__
+del _version
