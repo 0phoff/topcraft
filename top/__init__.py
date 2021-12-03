7c07634084784c8f0507a8577a69549e4c41b3b2
0phoff <0phoff@users.noreply.github.com>
2021-12-03 18:22:16 +0100

    del _files from init, so it doesnt pollute top scope

--- top/__init__.py
+++ top/__init__.py
@@ -1,7 +1,17 @@
 from ._gc import *
+del _gc
+
 from ._log import *
+del _log
+
 from ._memory import *
+del _memory
+
 from ._profile import *
+del _profile
+
 from ._time import *
+del _time
+
 
 from . import _meta as meta
